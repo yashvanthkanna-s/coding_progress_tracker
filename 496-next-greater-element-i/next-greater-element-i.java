c585d023f04c761103c75170cc35de466b79c082
yashvanthkanna-s <yashvanthkanna888@gmail.com>
2026-09-11 17:44:09 +0530

    Time: 3 ms (92.97%) | Memory: 44.7 MB (90.97%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -5,7 +5,7 @@ class Solution{
             ans[i]=-1;
             for(int j=0;j<nums2.length;j++){
            if(nums1[i]==nums2[j]){
-                    for(int k=j;k<nums2.length;k++){
+                    for(int k=j+1;k<nums2.length;k++){
                if(nums2[k]>nums1[i]){
                             ans[i]=nums2[k];
                             break;
