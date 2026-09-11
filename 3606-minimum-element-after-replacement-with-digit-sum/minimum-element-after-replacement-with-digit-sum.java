aa4724b34b48d5e652656a7c2d68e934be156387
yashvanthkanna-s <yashvanthkanna888@gmail.com>
2026-09-11 17:47:57 +0530

    Time: 1 ms (100.00%) | Memory: 44.9 MB (57.28%) - LeetSync

---
+++ 3606-minimum-element-after-replacement-with-digit-sum/minimum-element-after-replacement-with-digit-sum.java
@@ -0,0 +1,14 @@
+class Solution{
+    public int minElement(int[]nums){
+        int ans=Integer.MAX_VALUE;
+        for(int n:nums){
+            int sum=0;
+            while(n>0){
+                sum+=n%10;
+                n/=10;
+            }
+            ans=Math.min(ans,sum);
+        }
+ return ans;
+    }
+}
