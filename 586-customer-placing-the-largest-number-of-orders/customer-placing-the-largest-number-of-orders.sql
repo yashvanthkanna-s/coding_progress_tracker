44278062d7c40f27a8dcef5f7774d9f6fbc5a986
yashvanthkanna-s <yashvanthkanna888@gmail.com>
2026-09-15 18:03:51 +0530

    Time: 482 ms (75.18%) | Memory: 0B (100.00%) - LeetSync

--- 586-customer-placing-the-largest-number-of-orders/customer-placing-the-largest-number-of-orders.sql
+++ 586-customer-placing-the-largest-number-of-orders/customer-placing-the-largest-number-of-orders.sql
@@ -1,2 +1,2 @@
 select customer_number
-from  Orders group by customer_number order by COUNT(order_number) desc limit 1;
+from  Orders group by customer_number order by count(order_number) desc limit 1;
