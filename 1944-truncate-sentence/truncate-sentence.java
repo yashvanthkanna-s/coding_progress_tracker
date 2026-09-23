73761b865e462d1ab9f94a3fcc4c0fe3e7656e2c
yashvanthkanna-s <yashvanthkanna888@gmail.com>
2026-09-23 17:55:29 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (97.47%) - LeetSync

---
+++ 1944-truncate-sentence/truncate-sentence.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String truncateSentence(String s, int k) {
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)==' '){
+            k--;
+            }
+        
+            if(k==0){
+         return s.substring(0,i);
+        }
+    }
+            
+        
+        return s;
+    }
+}
